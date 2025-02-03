697076db8cff78462be689028f9d48ce9ab4f319
vKurama7u7v <aurelio.marin@iest.edu.mx>
2025-02-03 17:24:42 -0600

    [feat]: :wrench: configure dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import { ModeToggle } from '@/common/components'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="flex flex-col row-start-2 items-center gap-8 sm:items-start">
         <Image
           className="dark:invert"
@@ -12,10 +13,10 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            Get started by editing{' '}
+            <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
               src/app/page.tsx
             </code>
             .
@@ -25,7 +26,7 @@ export default function Home() {
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:h-12 sm:px-5 sm:text-base"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -40,7 +41,7 @@ export default function Home() {
             Deploy now
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -49,6 +50,9 @@ export default function Home() {
           </a>
         </div>
       </main>
+      <div className="">
+        <ModeToggle />
+      </div>
       <footer className="flex flex-wrap row-start-3 justify-center items-center gap-6">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -97,5 +101,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
--- src/common/provider/main-provider.tsx
+++ src/common/provider/main-provider.tsx
@@ -3,11 +3,7 @@ import { HeroUIProvider } from '@heroui/react'
 import React, { PropsWithChildren } from 'react'
 
 function MainProvider({ children }: PropsWithChildren) {
-  return (
-    <HeroUIProvider>
-      {children}
-    </HeroUIProvider>
-  )
+  return <HeroUIProvider>{children}</HeroUIProvider>
 }
 
 export default MainProvider
