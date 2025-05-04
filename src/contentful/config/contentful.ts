08c5b2d67fd081524180773854872fe679df7a6a
Aurelio Marín Bautista <aurelio.marin@iest.edu.mx>
2025-05-03 21:10:31 -0600

    [chore]: :heavy_plus_sign: install contentful dependencies

---
+++ src/contentful/config/contentful.ts
@@ -0,0 +1,8 @@
+import { createClient } from 'contentful'
+import { env } from '@/config' 
+
+export const contentfulClient = createClient({
+  space: env.CONTENTFUL_SPACE_ID,
+  accessToken: env.CONTENTFUL_ACCESS_TOKEN,
+  environment: env.CONTENTFUL_ENVIRONMENT,
+})
