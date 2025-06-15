00afbcc92940af9254344d0b7321d57d7098a394
vKurama7u7v <aurelio.marin@iest.edu.mx>
2025-06-15 11:52:54 -0600

    feat: :sparkles: add LanguageToggle component for language selection and localization support

--- src/common/components/preferences-toggle.tsx
+++ src/common/components/preferences-toggle.tsx
@@ -9,6 +9,8 @@ import {
 } from '@heroui/react'
 import { Moon, SettingsIcon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { GetIcon } from './icons'
+import { useTranslations } from 'next-intl'
 
 export const PreferencesButton = () => {
   const [color, setColor] = useState<string>('blue')
@@ -71,10 +73,12 @@ export const PreferencesButton = () => {
 export const ThemeToggle = () => {
   const { setTheme } = useTheme()
 
+  const t = useTranslations('Settings')
+
   return (
     <Dropdown className="w-fit min-w-fit">
       <DropdownTrigger>
-        <Button className="bg-custom-background-tertiary w-fit min-w-fit p-3 hover:bg-custom-background-secondary">
+        <Button className="w-fit min-w-fit bg-custom-background-tertiary p-3 hover:bg-custom-background-secondary">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 text-custom-primary transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 text-custom-primary transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
@@ -82,13 +86,13 @@ export const ThemeToggle = () => {
       </DropdownTrigger>
       <DropdownMenu closeOnSelect={true}>
         <DropdownItem key="light" onPress={() => setTheme('light')}>
-          Light
+          <div className="text-sm font-medium">{t('light')}</div>
         </DropdownItem>
         <DropdownItem key="dark" onPress={() => setTheme('dark')}>
-          Dark
+          <div className="text-sm font-medium">{t('dark')}</div>
         </DropdownItem>
         <DropdownItem key="system" onPress={() => setTheme('system')}>
-          System
+          <div className="text-sm font-medium">{t('system')}</div>
         </DropdownItem>
       </DropdownMenu>
     </Dropdown>
@@ -116,7 +120,7 @@ export const ColorToggle = ({ setColor }: ColorProps) => {
   return (
     <Dropdown className="w-fit min-w-fit">
       <DropdownTrigger>
-        <Button className="bg-custom-background-tertiary w-fit min-w-fit p-3 hover:bg-custom-background-secondary">
+        <Button className="w-fit min-w-fit bg-custom-background-tertiary p-3 hover:bg-custom-background-secondary">
           <div className="flex h-[1.2rem] w-[1.2rem] rotate-0 scale-100 items-center justify-center rounded-full bg-custom-primary text-custom-text-body transition-all">
             ✔︎
           </div>
@@ -132,6 +136,59 @@ export const ColorToggle = ({ setColor }: ColorProps) => {
               }}
               className="h-[1.2rem] w-[1.2rem] rounded-full"
             ></div>
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
+
+interface LanguageProps {
+  onSelectLanguage: (nextLocale: string) => void
+  locale: string
+  isPending: boolean
+}
+
+export const LanguageToggle = ({
+  onSelectLanguage,
+  locale,
+  isPending,
+}: LanguageProps) => {
+  const t = useTranslations('Settings')
+
+  const languages = [
+    { code: 'en', label: t('english'), flag: 'en-flag' },
+    { code: 'es', label: t('spanish'), flag: 'mx-flag' },
+  ]
+
+  return (
+    <Dropdown className="w-fit min-w-fit">
+      <DropdownTrigger className={isPending ? 'pointer-events-none' : ''}>
+        <Button
+          className="w-fit min-w-fit bg-custom-background-tertiary p-2 hover:bg-custom-background-secondary"
+          disabled={isPending}
+          isIconOnly
+        >
+          <GetIcon
+            name={locale === 'en' ? 'en-flag' : 'mx-flag'}
+            className="h-[1.5rem] w-[1.5rem]"
+          />
+          <span className="sr-only">Select Language</span>
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu closeOnSelect={true}>
+        {languages.map((lang) => (
+          <DropdownItem
+            key={lang.code}
+            onPress={() => onSelectLanguage(lang.code)}
+          >
+            <div className="flex items-center text-sm font-medium">
+              <GetIcon
+                name={lang.flag}
+                className="mr-2 inline-block h-[1.2rem] w-[1.2rem]"
+              />
+              {lang.label}
+            </div>
           </DropdownItem>
         ))}
       </DropdownMenu>
