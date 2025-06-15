a30ae2fa4d7fe92a68ebb9d8bc6d0b5c4eaf49cc
vKurama7u7v <aurelio.marin@iest.edu.mx>
2025-06-15 16:48:52 -0600

    refactor: :recycle: reorganize imports and improve code formatting in page.tsx for better readability

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,7 +8,11 @@ import {
   MyHistory,
   MyBento,
 } from '@/common/home'
-import { experienceService, profileService, servicesService } from '@/contentful/services'
+import {
+  experienceService,
+  profileService,
+  servicesService,
+} from '@/contentful/services'
 import { getContentfulLocale } from '@/utils'
 
 export default async function Home() {
@@ -19,7 +23,7 @@ export default async function Home() {
     '35s65bVaP0YFwaFApB1PAX',
     getContentfulLocale(locale)
   )
-  
+
   if (!retreiveProfile) {
     throw new Error('Profile not found')
   }
@@ -28,12 +32,17 @@ export default async function Home() {
 
   const { services, experience } = fields || {}
 
+  // console.log('Profile:', retreiveProfile)
+
   return (
     <BaseLayout>
-      <div className="w-full h-full gap-4 md:flex xl:gap-8">
+      <div className="h-full w-full gap-4 md:flex xl:gap-8">
         <ProfileSidebar data={retreiveProfile} />
         <div className="h-full w-full overflow-y-scroll scroll-smooth pt-[60px] scrollbar-hide md:pt-0">
-          <Header />
+          <Header
+            description={fields.shortDescription as any}
+            titles={fields.roleTitles as any}
+          />
           <Services services={services as any} />
           <MyHistory experiences={experience as any} />
           <MyBento />
