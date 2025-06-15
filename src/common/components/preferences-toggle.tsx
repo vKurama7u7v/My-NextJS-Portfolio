b4b11bb28d4f73fbdfe4e0fe96fab1c36b991e5b
vKurama7u7v <aurelio.marin@iest.edu.mx>
2025-06-15 11:57:07 -0600

    feat: :sparkles: update PreferencesButton to persist theme color in localStorage and enhance ColorToggle button styling

--- src/common/components/preferences-toggle.tsx
+++ src/common/components/preferences-toggle.tsx
@@ -13,7 +13,9 @@ import { GetIcon } from './icons'
 import { useTranslations } from 'next-intl'
 
 export const PreferencesButton = () => {
-  const [color, setColor] = useState<string>('blue')
+  const [color, setColor] = useState<string>(
+    localStorage.getItem('theme-color') || 'blue'
+  )
 
   useEffect(() => {
     onSetColor(color)
@@ -28,41 +30,13 @@ export const PreferencesButton = () => {
 
     if (!document.documentElement.classList.contains(color)) {
       document.documentElement.classList.add(`theme-${color}`)
+      localStorage.setItem('theme-color', color)
     }
 
     console.log(document.documentElement.classList)
   }
 
   return (
-    // <Dropdown className="w-fit min-w-fit">
-    //   <DropdownTrigger>
-    //     <Button
-    //       isIconOnly
-    //       className="h-8 w-8 min-w-8 bg-background text-[10px] text-custom-text-light hover:text-custom-primary"
-    //       radius="full"
-    //     >
-    //       <SettingsIcon className="w-4 h-4" />
-    //     </Button>
-    //   </DropdownTrigger>
-    //   <DropdownMenu closeOnSelect={false}>
-    //     <DropdownItem
-    //       className="bg-transparent hover:bg-transparent data-[hover=true]:bg-transparent"
-    //       key={'Theme Settings'}
-    //     >
-    //       <ThemeToggle />
-    //     </DropdownItem>
-    //     <DropdownItem
-    //       className="bg-transparent hover:bg-transparent data-[hover=true]:bg-transparent"
-    //       key={'Color Settings'}
-    //     >
-    //       <ColorToggle
-    //         color={color}
-    //         setColor={setColor}
-    //         onSetColor={onSetColor}
-    //       />
-    //     </DropdownItem>
-    //   </DropdownMenu>
-    // </Dropdown>
     <>
       <ThemeToggle />
       <ColorToggle color={color} setColor={setColor} onSetColor={onSetColor} />
@@ -121,7 +95,7 @@ export const ColorToggle = ({ setColor }: ColorProps) => {
     <Dropdown className="w-fit min-w-fit">
       <DropdownTrigger>
         <Button className="w-fit min-w-fit bg-custom-background-tertiary p-3 hover:bg-custom-background-secondary">
-          <div className="flex h-[1.2rem] w-[1.2rem] rotate-0 scale-100 items-center justify-center rounded-full bg-custom-primary text-custom-text-body transition-all">
+          <div className="flex h-[1.2rem] w-[1.2rem] rotate-0 scale-100 items-center justify-center rounded-full bg-custom-primary text-white transition-all">
             ✔︎
           </div>
           <span className="sr-only">✔︎</span>
