0ac2e45ea339c26dd4b89c6243af619b674845ef
vKurama7u7v <aurelio.marin@iest.edu.mx>
2025-06-14 19:10:05 -0600

    feat: :sparkles: enhance date formatting functions to handle date string normalization

--- src/utils/date-formater.ts
+++ src/utils/date-formater.ts
@@ -2,15 +2,22 @@ import { format } from '@formkit/tempo'
 
 export const formatDate = (
   date: string | Date,
-  formatString: 'full' | 'long' | 'medium' | 'short' = 'medium',
+  formatString: 'full' | 'long' | 'medium' | 'short' | string = 'medium',
   locale: 'es' | 'en' = 'es'
 ) => {
+  let newDate = date.toString().split('-')
+  if (newDate.length === 3) {
+    newDate = newDate.map((part) => part.padStart(2, '0'))
+    date = `${newDate[0]}-${newDate[1]}-${newDate[2]}T00:00:00`
+  } else if (typeof date === 'string' && !isNaN(Date.parse(date))) {
+    date = new Date(date)
+  }
   return format(new Date(date), formatString, locale)
 }
 
 export const formatDateTime = (
   date: string | Date,
-  formatString: 'full' | 'long' | 'medium' | 'short' = 'medium',
+  formatString: 'full' | 'long' | 'medium' | 'short' | string = 'medium',
   locale: 'es' | 'en' = 'es'
 ) => {
   return format(new Date(date), formatString, locale)
