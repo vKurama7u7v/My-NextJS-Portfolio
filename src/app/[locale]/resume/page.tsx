5656e30791043bf343a88cca53246f430006fb2c
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2025-07-20 22:13:40 -0600

    feat: :sparkles: update Resume page to include PokemonWidget component

--- src/app/[locale]/resume/page.tsx
+++ src/app/[locale]/resume/page.tsx
@@ -1,4 +1,5 @@
 import { BaseLayout } from '@/common/layouts'
+import PokemonWidget from '@/common/widgets/pokemon-widget'
 import { getTranslations } from 'next-intl/server'
 
 const Resume = async () => {
@@ -6,9 +7,8 @@ const Resume = async () => {
   return (
     <BaseLayout>
       <div className="h-full w-full gap-4 overflow-y-scroll scroll-smooth pt-[60px] scrollbar-hide md:flex md:pt-0 xl:gap-8">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae maxime
-        dolorem aliquid iste et provident iusto repellat sit voluptatum ipsum!
-        Sed cum ipsam recusandae cupiditate fugit quae asperiores modi quos.
+        <div className="">Pokemon</div>
+        <PokemonWidget />
       </div>
     </BaseLayout>
   )
