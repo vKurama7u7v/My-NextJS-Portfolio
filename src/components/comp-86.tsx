c4566f14575b813878e8f506a9c0eaec86d4c602
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2025-07-21 21:53:49 -0600

    style: :art: standardize import quotes in comp-86.tsx

--- src/components/comp-86.tsx
+++ src/components/comp-86.tsx
@@ -1,6 +1,6 @@
-import { ArrowRightIcon } from "lucide-react"
+import { ArrowRightIcon } from 'lucide-react'
 
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button'
 
 export default function Component() {
   return (
