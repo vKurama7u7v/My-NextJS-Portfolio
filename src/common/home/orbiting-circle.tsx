baf35b0ea76e472afc6cc884f3918c42e2deb7cc
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2025-07-26 13:26:31 -0600

    refactor: :recycle: restructure OrbitingCircleComponent for improved layout and readability

--- src/common/home/orbiting-circle.tsx
+++ src/common/home/orbiting-circle.tsx
@@ -1,34 +1,39 @@
 import { OrbitingCircles } from '@/components/magicui/orbiting-circles'
-import { GetBrand } from '../components'
+import { GetBrand, Memoji } from '../components'
 
 export const OrbitingCircleComponent = () => {
   return (
-    <div className="flex h-[200px] w-[200px] items-center justify-center overflow-hidden">
-      <OrbitingCircles>
-        <GetBrand
-          name="github"
-          className="h-12 w-12 text-black dark:text-white"
-        />
-        <GetBrand name="docker" className="h-12 w-12" />
-        <GetBrand
-          name="vercel"
-          className="h-12 w-12 text-black dark:text-white"
-        />
-        <GetBrand name="figma" className="h-7 w-12" />
-        <GetBrand name="supabase" className="h-12 w-12" />
-        <GetBrand name="notion" className="h-12 w-12" />
-        <GetBrand
-          name="prisma"
-          className="h-12 w-12 text-black dark:text-white"
-        />
-      </OrbitingCircles>
-      <OrbitingCircles radius={65} reverse>
-        <GetBrand name="nextjs" className="h-12 w-12" />
-        <GetBrand name="tailwind" className="h-12 w-12" />
-        <GetBrand name="react" className="h-12 w-12" />
-        <GetBrand name="nestjs" className="h-12 w-12" />
-        <GetBrand name="shopify" className="h-12 w-12" />
-      </OrbitingCircles>
-    </div>
+    <>
+      <div className="relative flex h-full w-full items-center justify-center overflow-hidden">
+        <OrbitingCircles>
+          <GetBrand
+            name="github"
+            className="h-12 w-12 text-black dark:text-white"
+          />
+          <GetBrand name="docker" className="h-12 w-12" />
+          <GetBrand
+            name="vercel"
+            className="h-12 w-12 text-black dark:text-white"
+          />
+          <GetBrand name="figma" className="h-7 w-12" />
+          <GetBrand name="supabase" className="h-12 w-12" />
+          <GetBrand name="notion" className="h-12 w-12" />
+          <GetBrand
+            name="prisma"
+            className="h-12 w-12 text-black dark:text-white"
+          />
+        </OrbitingCircles>
+        <OrbitingCircles radius={70} reverse>
+          <GetBrand name="nextjs" className="h-12 w-12" />
+          <GetBrand name="tailwind" className="h-12 w-12" />
+          <GetBrand name="react" className="h-12 w-12" />
+          <GetBrand name="nestjs" className="h-12 w-12" />
+          <GetBrand name="shopify" className="h-12 w-12" />
+        </OrbitingCircles>
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <Memoji />
+      </div>
+    </>
   )
 }
